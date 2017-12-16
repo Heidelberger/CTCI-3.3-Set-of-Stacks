ee0e5892ef842e31c887e07c089a87b492816fb7
Qubert <upafourth@gmail.com>
2017-12-16 10:40:37 +0100

    Add project files.

---
+++ CTCI 3.3 Set of Stacks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_3._3_Set_of_Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
